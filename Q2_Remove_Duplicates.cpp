bcd537916306d5b3245b94a484a8478f6cd8f913
Nikait215 <nikait017@gmail.com>
2026-08-24 02:01:28 +0530

    Q2_Remove_Duplicates.cpp

---
+++ Q2_Remove_Duplicates.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100];
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements:\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Removing duplicate elements
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                // Shift elements to the left
+                for (int k = j; k < n - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+
+                n--;
+                j--;
+            }
+        }
+    }
+
+    cout << "\nArray after removing duplicate elements:\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
